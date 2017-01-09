8270ad3a79aaef91bea9e7f7882cfe0e05329a4e
Mark C Mitchell <mark@markcmitchell.net>
2017-01-09 17:50:51 +1100

    add src/music.js

---
+++ src/music.js
@@ -0,0 +1,51 @@
+import insertWidget from './mixcloud-widget'
+
+let music = (() => {
+ 
+  let hasHadFirstPlay = false
+  let widget
+
+  return {
+    load: url => {
+      widget = insertWidget(url)
+    },
+
+    play: () => {
+      if(!widget) {
+        console.error('Mixcloud has not been set! Please call load() with url to the cloudcast.')
+        return
+      }
+
+      widget.ready.then(() => {
+        if(!hasHadFirstPlay) {
+          widget.events.play.on(onPlay)
+          
+          function onPlay() {
+            seekTo.call(this, 12) // magic number, explain
+            widget.events.play.off(onPlay)
+          }
+          function seekTo(seconds) {
+            widget.seek(seconds)
+          }
+          hasHadFirstPlay = true
+        }
+
+        widget.play()
+      })
+    },
+
+    pause: () => {
+      if(!widget) {
+        console.error('Mixcloud has not been set! Please call set() with url to cloudcast.')
+        return
+      }
+
+      widget.ready.then(() => {
+        widget.pause()
+      })
+    }
+  } 
+
+})()
+
+export { music }
