bf870329244d8c112c1537bb2535cfe81f64dd63
Mark C Mitchell <mark@markcmitchell.net>
2017-01-10 17:23:44 +1100

    server app using express, socket.io and babelify middleware. Uses snowboy hotword detection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import grab from './do-you-grabber'
-import { music } from './music'
+import snowboy from 'snowboy'
+import { detector } from './snowboy-setup'
+import express from 'express'
+import ioServer from 'socket.io'
+import babelify from 'express-babelify-middleware'
 
+let app = express()
+const http = require('http').Server(app)
+const io = ioServer(http)
 
-grab().then(url => {
+// babelify middleware
+app.use('/index.js', babelify('src/client/index.js'))
 
-  music.load(url)
-  music.play()
+app.use(express.static('src/client/'))
 
+detector.on('hotword', (index, hotword) => {
+  console.log('got hotword!')
+  io.emit('hotword')
+})
+
+http.listen(3000, () => {
+  console.log('server listening on 3000')
 })
